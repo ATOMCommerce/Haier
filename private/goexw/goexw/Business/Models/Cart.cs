d994e85690a37d46870e40645ff2f6b2fb0fe256
yzhao2015 <yzhao@atom-commerce.com>
2014-12-09 18:52:45 -0800

    Add the cart model.

---
+++ private/goexw/goexw/Business/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Goexw.Business.Models
+{
+    public class Cart
+    {
+        public string UserId { get; set; }
+
+        public string AuthType { get; set; }
+
+        public IList<SalesLine> SalesLines { get; set; }
+    }
+}
